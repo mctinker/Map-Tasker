bbfea998c3bad41792bee82f42a23f5ff77f573f
mctinker <mikrubin@gmail.com>
2022-05-25 13:38:12 -0400

    MapTasker version 1.1
    Version 1.1: Added list of Tasks not associated with any Profile

--- MapTasker.py
+++ MapTasker.py
@@ -1,13 +1,26 @@
 #! /usr/bin/env python3
+
+# ########################################################################################## #
+#                                                                                            #
+# MapTasker: read the Tasker backup file to map out the configuration                        #
+#                                                                                            #
+# Version 1.1                                                                                #
+#                                                                                            #
+# - 1.1 Added list of Tasks for which there is no Profile                                    #
+#                                                                                            #
+# ########################################################################################## #
+
 import easygui as g
 import xml.etree.ElementTree as ET
 import os
+import sys, getopt
 
 #  Global constants
 project_color = 'Black'
 profile_color = 'Blue'
 task_color = 'Green'
 scene_color = 'Purple'
+unknown_task_name = 'Unnamed or Anonymous!!!'
 
 
 def ulify(element, lvl=int):
@@ -38,20 +51,20 @@ def my_output(out_file, list_level, out_string):
 # Main program here >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>        
 def main():
     task_list = []
-    have_task = ''
+    all_tasks = []
 
     # Find the Tasker backup.xml
     msg = 'Locate the Tasker backup xml file to use to map your Tasker environment'
     title = 'MapTasker'
     filename = g.fileopenbox(msg, title, default="*.xml", multiple=False)
     if filename is None:
-        g.textbox(msg="MapTasker cancelled",title="MapTasker",text="Backup selection cancelled.  Program ended.")
+        g.textbox(msg="MapTasker cancelled", title="MapTasker", text="Backup selection cancelled.  Program ended.")
         exit(1)
 
     # Store the output in the same directory
     my_output_dir = os.getcwd()
     if my_output_dir is None:
-        g.textbox(msg="MapTasker cancelled",title="MapTasker",text="An error occurred.  Program cancelled.")
+        g.textbox(msg="MapTasker cancelled", title="MapTasker", text="An error occurred.  Program cancelled.")
         exit(1)
     out_file = open("MapTasker.html", "w")
 
@@ -94,6 +107,7 @@ def main():
                                 task_id = child.text
                                 for task in root.findall('Task'):
                                     if task_id == task.find('id').text:
+                                        all_tasks.append(task_id)
                                         try:
                                             task_name = task.find('nme').text
                                             if have_task:
@@ -103,9 +117,9 @@ def main():
                                                 have_task = '1'
                                         except Exception as e:
                                             if have_task:
-                                                task_list.append('Unnamed/Anonymous!!! <<< Exit task')
+                                                task_list.append(unknown_task_name + ' <<< Exit task')
                                             else:
-                                                task_list.append('Unnamed/Anonymous !!!')
+                                                task_list.append(unknown_task_name)
                                                 have_task = '1'
                                             break
                         try:
@@ -136,9 +150,54 @@ def main():
         my_output(out_file, 3, '')  # Close Profile list
     my_output(out_file, 3, '')  # Close Project list
 
+    # Now let's look for Tasks that are not referenced by Profiles
+    first_task = 1
+    task_name = ''
+    unnamed_tasks = 0
+    for task in root.findall('Task'):  # Get a/next Task
+        task_id = task.find('id').text
+        if task_id in all_tasks:
+            pass
+        else:  # We have a solo Task
+            project_name = '(None)'  # Find the Project it belongs to
+            for project in root.findall('Project'):
+                project_tasks = project.find('tids').text
+                if task_id in project_tasks:
+                    project_name = project.find('name').text
+            if first_task == 1:
+                my_output(out_file, 0, 'Tasks that are not in any Profile (may be a desktop Tasker Task/widget)...')
+                my_output(out_file, 1, '')  # Start Task list
+            try:
+                task_name = task.find('nme').text
+            except Exception as e:  # Task name not found!
+                unnamed_tasks += 1
+                pass
+                # For future use
+                task_name = unknown_task_name
+                try:
+                    action = task.find('Action')
+                    for child in action:
+                        if 'Str' == child.tag and child.text != 'None':
+                            if '%' == child.text[0:1]:  # junk?
+                                break
+                            task_name += '  The first Task action is >>> ' + child.text
+                            break
+                except Exception as e:
+                    pass
+
+            task_name += ' ...in Project ' + project_name
+            if unknown_task_name not in task_name:
+                my_output(out_file, 2, 'Task: ' + task_name)
+            first_task = 0
+    if unnamed_tasks > 0:
+        my_output(out_file, 2, 'There are ' + str(unnamed_tasks) + ' unnamed Tasks!!!')
+    if task_name is True:
+        my_output(out_file, 3, '')  # Close Task list
+
     my_output(out_file, 3, '')  # Close out the list
     out_file.close()
-    g.textbox(msg="MapTasker Done",title="MapTasker",text="You can find 'MapTasker.html' in the same folder as the backup.xml file is located.  Open it with a browser.")
+    g.textbox(msg="MapTasker Done", title="MapTasker",
+              text="You can find 'MapTasker.html' in the same folder as the backup.xml file is located.  Open it with a browser.")
 
 
 # Main call
