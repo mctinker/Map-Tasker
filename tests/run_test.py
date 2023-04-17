145ef11362f69c10e135690b2184ef3522d01772
mctinker <mikrubin@gmail.com>
2023-04-17 12:31:24 -0400

    Version 1.3.3

--- tests/run_test.py
+++ tests/run_test.py
@@ -19,36 +19,47 @@ import sys
 
 
 def test_main():
-    # # Test full detail
-    # with patch("sys.argv", ["-test=yes", "detail=3", "d"]):
-    #     print('run_test sys.argv:', sys.argv)
-    #     mapit.mapit_all()
-    # # Test limited detail
-    # with patch("sys.argv", ["-test=yes", "detail=2", "d"]):
-    #     print('run_test sys.argv:', sys.argv)
-    #     mapit.mapit_all()
-    # # Test by Project name
-    # with patch(
-    #     "sys.argv", ["-test=yes", "project=Base", "d", "conditions", "taskernet"]
-    # ):
-    #     print('run_test sys.argv:', sys.argv)
-    #     mapit.mapit_all()
-    # # Test by Profile name
-    # with patch(
-    #     "sys.argv",
-    #     ["-test=yes", "profile=Check Smartthings Batteries", "detail=3", "d"],
-    # ):
-    #     print('run_test sys.argv:', sys.argv)
-    #     mapit.mapit_all()
-    # # Test by Task name
-    # with patch("sys.argv", ["-test=yes", "task=Check Batteries", "d"]):
-    #     print('run_test sys.argv:', sys.argv)
-    #     mapit.mapit_all()
+    # Test full detail
+    with patch("sys.argv", ["-test=yes", "detail=3", "d"]):
+        print('run_test sys.argv:', sys.argv)
+        mapit.mapit_all()
+    # Test limited detail
+    with patch("sys.argv", ["-test=yes", "detail=2", "d"]):
+        print('run_test sys.argv:', sys.argv)
+        mapit.mapit_all()
+    # Test limited detail 1
+    with patch("sys.argv", ["-test=yes", "detail=1", "d"]):
+        print('run_test sys.argv:', sys.argv)
+        mapit.mapit_all()
+    # Test no detail
+    with patch("sys.argv", ["-test=yes", "detail=0", "d"]):
+        print('run_test sys.argv:', sys.argv)
+        mapit.mapit_all()
+    # Test by Project name
+    with patch(
+        "sys.argv", ["-test=yes", "project=Base", "d", "conditions", "taskernet"]
+    ):
+        print('run_test sys.argv:', sys.argv)
+        mapit.mapit_all()
+    # Test by Profile name
+    with patch(
+        "sys.argv",
+        ["-test=yes", "profile=Check Smartthings Batteries", "detail=3", "d"],
+    ):
+        print('run_test sys.argv:', sys.argv)
+        mapit.mapit_all()
+    # Test by Task name
+    with patch("sys.argv", ["-test=yes", "task=Check Batteries", "d"]):
+        print('run_test sys.argv:', sys.argv)
+        mapit.mapit_all()
     # Test new -pref
     with patch("sys.argv", ["-test=yes", "p", "d", "taskernet", "detail=2"]):
         mapit.mapit_all()
     # Test new -everything
     with patch("sys.argv", ["-test=yes", "e"]):
+        mapit.mapit_all()
+    # Test colors
+    with patch("sys.argv", ["-test=yes", "cBackground=Black", "d"]):
         mapit.mapit_all()
 
 
