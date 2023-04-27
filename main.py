044b612b33891ccf4890d7f600c6854ed759a5e4
mctinker <mikrubin@gmail.com>
2023-04-27 15:21:24 -0400

    Version 1.3.4
    Signed-off-by: mctinker <mikrubin@gmail.com>

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+#! /usr/bin/env python3
+
+# ########################################################################################## #
+#                                                                                            #
+# Main: MapTasker entry point                                                                #
+#                                                                                            #
+# GNU General Public License v3.0                                                            #
+# Permissions of this strong copyleft license are conditioned on making available            #
+# complete source code of licensed works and modifications, which include larger works       #
+# using a licensed work, under the same license. Copyright and license notices must be       #
+# preserved. Contributors provide an express grant of patent rights.                         #
+#                                                                                            #
+# ########################################################################################## #
+
 from maptasker.src import mapit
 
 
@@ -5,7 +19,9 @@ def main():
     """
     Kick off the main program: mapit.py
     """
-    return_code = mapit.mapit_all()
+
+    # Call the core function passing an empty filename
+    return_code = mapit.mapit_all("")
     exit(return_code)
 
 
