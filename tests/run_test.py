0997c51e0b11df7eb378b333a38329e53b69a069
mctinker <mikrubin@gmail.com>
2023-08-09 09:58:17 -0400

    Version 2.1.0
    Signed-off-by: mctinker <mikrubin@gmail.com>

--- tests/run_test.py
+++ tests/run_test.py
@@ -1,21 +1,22 @@
 #! /usr/bin/env python3
 
-# ########################################################################################## #
-#                                                                                            #
-# run_test: run MapTasker unit test routines                                                 #
-#                                                                                            #
-# GNU General Public License v3.0                                                            #
-# Permissions of this strong copyleft license are conditioned on making available            #
-# complete source code of licensed works and modifications, which include larger works       #
-# using a licensed work, under the same license. Copyright and license notices must be       #
-# preserved. Contributors provide an express grant of patent rights.                         #
-#                                                                                            #
-# Reference: https://github.com/Taskomater/Tasker-XML-Info                                   #
-#                                                                                            #
-# ########################################################################################## #
-from unittest.mock import patch
-from maptasker.src import mapit
+# #################################################################################### #
+#                                                                                      #
+# run_test: run MapTasker unit test routines                                           #
+#                                                                                      #
+# GNU General Public License v3.0                                                      #
+# Permissions of this strong copyleft license are conditioned on making available      #
+# complete source code of licensed works and modifications, which include larger works #
+# using a licensed work, under the same license. Copyright and license notices must be #
+# preserved. Contributors provide an express grant of patent rights.                   #
+#                                                                                      #
+# Reference: https://github.com/Taskomater/Tasker-XML-Info                             #
+#                                                                                      #
+# #################################################################################### #
 import time
+from unittest.mock import patch
+
+from maptasker.src import mapit
 
 
 def test_it():
@@ -25,119 +26,133 @@ def test_it():
     time.sleep(1)
 
 
+# Run these in small chunks, depending on the size of the backup file being used.
 def test_main():
-    # Test full detail
-    with patch("sys.argv", ["-test=yes", "detail=3", "d"]):
-        test_it()
-    # Test limited detail
-    with patch("sys.argv", ["-test=yes", "detail=2", "d"]):
-        test_it()
-    # Test limited detail 1
-    with patch("sys.argv", ["-test=yes", "detail=1", "d"]):
-        test_it()
-    # Test no detail
-    with patch("sys.argv", ["-test=yes", "detail=0", "d"]):
-        test_it()
-    # Test by Project name
-    with patch(
-        "sys.argv", ["-test=yes", "project=Base", "d", "conditions", "taskernet"]
-    ):
-        test_it()
-    # Test by Profile name
-    with patch(
-        "sys.argv",
-        ["-test=yes", "profile=Check Smartthings Batteries", "detail=3", "d"],
-    ):
-        test_it()
-    # Test by Task name
-    with patch("sys.argv", ["-test=yes", "task=Check Batteries", "d"]):
-        test_it()
-    # Test -pref
-    with patch("sys.argv", ["-test=yes", "p", "d", "taskernet", "detail=2"]):
-        test_it()
-    # Test -dir
-    with patch("sys.argv", ["-test=yes", "directory", "d", "taskernet", "detail=3"]):
-        test_it()
-    # Test new -everything
-    with patch("sys.argv", ["-test=yes", "e", "twisty"]):
-        test_it()
-    # Test -b fetch backup xml file
-    with patch(
-        "sys.argv",
-        [
-            "-test=yes",
-            "b=http://192.168.0.210:1821+/Tasker/configs/user/backup.xml",
-        ],
-    ):
-        test_it()
-    # Test -twisty
-    with patch("sys.argv", ["-test=yes", "sys.argv", "d", "twisty"]):
-        test_it()
-    # Test colors
-    with patch(
-        "sys.argv",
-        [
-            "-test=yes",
-            "cBackground=Black",
-            "cActionCondition=Yellow",
-            "cProfileCondition=Red",
-            "cActionLabel=White",
-            "e",
-            "d",
-        ],
-    ):
-        test_it()
-    with patch(
-        "sys.argv",
-        [
-            "-test=yes",
-            "cProfile=Yellow",
-            "cDisabledAction=Green",
-            "cLauncherTask=Red",
-            "cActionName=White",
-            "e",
-            "d",
-        ],
-    ):
-        test_it()
-    with patch(
-        "sys.argv",
-        [
-            "-test=yes",
-            "cTask=Yellow",
-            "cUnknownTask=Green",
-            "cScene=Red",
-            "cTaskerNetInfo=White",
-            "e",
-            "d",
-        ],
-    ):
-        test_it()
-    with patch(
-        "sys.argv",
-        [
-            "-test=yes",
-            "cProfile=Yellow",
-            "cDisabledProfile=Orange",
-            "cBullet=Red",
-            "cPreferences=White",
-            "e",
-            "d",
-        ],
-    ):
-        test_it()
-    with patch(
-        "sys.argv",
-        [
-            "-test=yes",
-            "cTask=Yellow",
-            "cAction=Red",
-            "cTrailingComments=Yellow",
-            "e",
-            "d",
-        ],
-    ):
-        test_it()
+    # # Test name attributes
+    # with patch("sys.argv", ["-test=yes", "detail=2", "debug", "names=bold highlight", "cHighlight LightBlue"]):
+    #     test_it()
+    # with patch(
+    #     "sys.argv", ["-test=yes", "detail=2", "debug", "names=underline italicize"]
+    # ):
+    #     test_it()
+    # # Test light mode
+    # with patch("sys.argv", ["-test=yes", "detail=2", "debug", "a=light"]):
+    #     test_it()
+    # # Test bold highlight ands directory
+    # with patch("sys.argv", ["-test=yes", "detail=2", "debug", "names=bold highlight", "directory"]):
+    #     test_it()
+    # # Test full detail
+    # with patch("sys.argv", ["-test=yes", "detail=3", "debug"]):
+    #     test_it()
+    # # Test limited detail
+    # with patch("sys.argv", ["-test=yes", "detail=2", "debug"]):
+    #     test_it()
+    # # Test limited detail 1
+    # with patch("sys.argv", ["-test=yes", "detail=1", "debug"]):
+    #     test_it()
+    # # Test no detail
+    # with patch("sys.argv", ["-test=yes", "detail=0", "debug"]):
+    #     test_it()
+    # # Test by Project name
+    # with patch(
+    #     "sys.argv", ["-test=yes", "project=Base", "debug", "conditions", "taskernet"]
+    # ):
+    #     test_it()
+    # # Test by Profile name
+    # with patch(
+    #     "sys.argv",
+    #     ["-test=yes", "profile=Check Smartthings Batteries", "detail=3", "debug"],
+    # ):
+    #     test_it()
+    # # Test by Task name
+    # with patch("sys.argv", ["-test=yes", "task=Check Batteries", "debug"]):
+    #     test_it()
+    # # Test -pref
+    # with patch("sys.argv", ["-test=yes", "p", "debug", "taskernet", "detail=2"]):
+    #     test_it()
+    # # Test -dir
+    # with patch("sys.argv", ["-test=yes", "directory", "debug", "taskernet", "detail=3"]):
+    #     test_it()
+    # # Test new -everything
+    # with patch("sys.argv", ["-test=yes", "e", "twisty"]):
+    #     test_it()
+    # # Test -b fetch backup xml file
+    # with patch(
+    #     "sys.argv",
+    #     [
+    #         "-test=yes",
+    #         "b=http://192.168.0.210:1821+/Tasker/configs/user/backup.xml",
+    #     ],
+    # ):
+    #     test_it()
+    # # Test -twisty
+    # with patch("sys.argv", ["-test=yes", "sys.argv", "debug", "twisty"]):
+    #     test_it()
+    # # Test colors
+    # with patch(
+    #     "sys.argv",
+    #     [
+    #         "-test=yes",
+    #         "cBackground=Black",
+    #         "cActionCondition=Yellow",
+    #         "cProfileCondition=Red",
+    #         "cActionLabel=White",
+    #         "e",
+    #         "debug",
+    #     ],
+    # ):
+    #     test_it()
+    # with patch(
+    #     "sys.argv",
+    #     [
+    #         "-test=yes",
+    #         "cProfile=Yellow",
+    #         "cDisabledAction=Green",
+    #         "cLauncherTask=Red",
+    #         "cActionName=White",
+    #         "e",
+    #         "debug",
+    #     ],
+    # ):
+    #     test_it()
+    # with patch(
+    #     "sys.argv",
+    #     [
+    #         "-test=yes",
+    #         "cTask=Yellow",
+    #         "cUnknownTask=Green",
+    #         "cScene=Red",
+    #         "cTaskerNetInfo=White",
+    #         "e",
+    #         "debug",
+    #     ],
+    # ):
+    #     test_it()
+    # with patch(
+    #     "sys.argv",
+    #     [
+    #         "-test=yes",
+    #         "cProfile=Yellow",
+    #         "cDisabledProfile=Orange",
+    #         "cBullet=Red",
+    #         "cPreferences=White",
+    #         "e",
+    #         "debug",
+    #     ],
+    # ):
+    #     test_it()
+    # with patch(
+    #     "sys.argv",
+    #     [
+    #         "-test=yes",
+    #         "cTask=Yellow",
+    #         "cAction=Red",
+    #         "cTrailingComments=Yellow",
+    #         "e",
+    #         "debug",
+    #     ],
+    # ):
+    #     test_it()
 
 
 if __name__ == "__main__":
