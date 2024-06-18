85f4ff6194c3c173b4f8efae0bcd10cadf55b946
mctinker <mikrubin@gmail.com>
2024-06-18 10:56:45 -0400

    Version 4.0.12
    Signed-off-by: mctinker <mikrubin@gmail.com>

--- maptasker/src/sysconst.py
+++ maptasker/src/sysconst.py
@@ -18,7 +18,7 @@ import darkdetect
 # Global constants
 UNKNOWN_TASK_NAME = "Unnamed/Anonymous."
 
-VERSION = "4.0.11"
+VERSION = "4.0.12"
 MY_VERSION = f"MapTasker version {VERSION}"
 
 MY_LICENSE = "MIT License"
