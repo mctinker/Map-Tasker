1c1f1668ad43521ece57e49c822627d09331493a
mctinker <mikrubin@gmail.com>
2025-07-28 15:07:26 -0400

    Version 8.1.2
    Signed-off-by: mctinker <mikrubin@gmail.com>

--- misc_utilities/check_dependencies.py
+++ misc_utilities/check_dependencies.py
@@ -15,7 +15,9 @@ def check_dependency_updates(dependencies: list) -> None:
         match = re.match(r"([a-zA-Z0-9_-]+)([<>=~!]=?)(.+)?", dep)
         if match:
             package_name = match.group(1)
-            operator = match.group(2) if match.group(2) else "=="  # Default to exact match if no operator
+            operator = (
+                match.group(2) if match.group(2) else "=="
+            )  # Default to exact match if no operator
             current_version_str = match.group(3) if match.group(3) else None
 
             try:
@@ -64,7 +66,8 @@ dependencies = [
     "defusedxml>=0.7.1",  # More secure xml parser
     "google-generativeai>=0.8.5",  #  Ai Google Generative support
     "ollama>=0.5.1",  #  Ai Ollama support > cria rquires this
-    "openai>=1.93.0",  #  Ai OpenAi support
+    "openai>=1.95.0",  #  Ai OpenAi support
+    "packaging>=25.0",  # Customtkinter needs this
     "pillow==11.3.0",  # Image support in GUI.  Revert back to 11.2.0 to avoid UV bug with tkinter.
     "psutil>=7.0.0",  #  System monitoring
     "requests>=2.32.4",  # HTTP Server function request
