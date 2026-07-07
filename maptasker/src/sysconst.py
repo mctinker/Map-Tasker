994648b6423200ea7a8e59850b6030204fa17081
mctinker <mikrubin@gmail.com>
2026-07-07 13:37:56 -0400

    11.0.0b1 complete.

--- maptasker/src/sysconst.py
+++ maptasker/src/sysconst.py
@@ -17,7 +17,7 @@ import darkdetect
 
 # Global constants
 UNNAMED_ITEM = "(Unnamed)"
-VERSION = "11.0.0-beta-0"
+VERSION = "11.0.0b1"
 MY_VERSION = f"MapTasker version {VERSION}"
 
 MY_LICENSE = "MIT License"
