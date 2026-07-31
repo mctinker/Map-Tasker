0683a3c5f4b4d61766b3e4cfc58229025dd16256
mctinker <mikrubin@gmail.com>
2026-07-31 13:57:30 -0400

    Version was incorrect for 12.0.1

--- maptasker/src/sysconst.py
+++ maptasker/src/sysconst.py
@@ -17,7 +17,7 @@ import darkdetect
 
 # Global constants
 UNNAMED_ITEM = "(Unnamed)"
-VERSION = "12.0.0"
+VERSION = "12.0.1"
 MY_VERSION = f"MapTasker version {VERSION}"
 
 MY_LICENSE = "MIT License"
